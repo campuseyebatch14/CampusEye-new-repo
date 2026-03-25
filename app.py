9031cd9de419e303f811ceaa783b368abc3cdaa8
campuseyebatch14 <campuseyebatch14@gmail.com>
2026-03-25 23:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -182,4 +182,4 @@ def chat():
     return jsonify({'success': True, 'answer': "Chatbot ready."})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=1000)
